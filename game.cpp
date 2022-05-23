871245298a37a559d35b7461a3e4b647f7c6310c
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2022-05-23 21:34:42 +0530

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -67,3 +67,48 @@ void player_turn(){
 
     display_board();
 }
+bool gameover(){
+
+    for(int i=0; i<3; i++)
+    if(Board[i][0] == Board[i][1] && Board[i][0] == Board[i][2] || Board[0][i] == Board[1][i] && Board[0][i] == Board[2][i])
+    return false;
+
+
+
+    if(Board[0][0] == Board[1][1] && Board[0][0] == Board[2][2] || Board[0][2] == Board[1][1] && Board[0][2] == Board[2][0])
+    return false;
+
+
+    for(int i=0; i<3; i++)
+    for(int j=0; j<3; j++)
+    if(Board[i][j] != 'X' && Board[i][j] != 'O')
+    return true;
+
+
+    draw = true;
+    return false;
+}
+
+
+
+int main()
+{
+    cout<<"\t\t\t\t\tWELCOME\n";
+	cout<<"\t\t\tT I C K -- T A C -- T O E -- G A M E\t\t\t";
+    cout<<"\n\t\t\t\t2 PLAYER MODE\n\t\t\t";
+    display_board();
+    while(gameover()){
+
+        player_turn();
+
+    }
+    //if someone wins then there must be a change of turn  after update
+    if(turn == 'O' && draw == false){
+        cout<<"\n\nCongratulations!Player with 'X' has won the game";
+    }
+    else if(turn == 'X' && draw == false){
+        cout<<"\n\nCongratulations!Player with 'O' has won the game";
+    }
+    else
+    cout<<"\n\nGAME DRAW!!!\n\n";
+}
