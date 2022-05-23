26fa8f96ca8d582c15eec54a3179afe2d92c013f
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2022-05-23 21:32:49 +0530

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,1 +1,11 @@
-//Hello this is the main file.
+#include <iostream>
+#include <stdlib.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+char Board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+
+int choice;
+int row,column;
+char turn = 'X';
+bool draw = false;
