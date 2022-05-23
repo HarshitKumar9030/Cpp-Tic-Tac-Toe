96cee3a2c8a493792a5777bca519a55c259ee956
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2022-05-23 21:33:50 +0530

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -9,3 +9,19 @@ int choice;
 int row,column;
 char turn = 'X';
 bool draw = false;
+//Display Board
+void display_board(){
+
+
+
+    cout<<"PLAYER - 1 [X]\t PLAYER - 2 [O]\n\n";
+    cout<<"\t\t\t\t     |     |     \n";
+    cout<<"\t\t\t\t  "<<Board[0][0]<<"  |  "<<Board[0][1]<<"  |  "<<Board[0][2]<<" \n";
+    cout<<"\t\t\t\t_____|_____|_____\n";
+    cout<<"\t\t\t\t     |     |     \n";
+    cout<<"\t\t\t\t  "<<Board[1][0]<<"  |  "<<Board[1][1]<<"  |  "<<Board[1][2]<<" \n";
+    cout<<"\t\t\t\t_____|_____|_____\n";
+    cout<<"\t\t\t\t     |     |     \n";
+    cout<<"\t\t\t\t  "<<Board[2][0]<<"  |  "<<Board[2][1]<<"  |  "<<Board[2][2]<<" \n";
+    cout<<"\t\t\t\t     |     |     \n";
+}
