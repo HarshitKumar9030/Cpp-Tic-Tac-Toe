49cc368812d17fad77113044e235cba6a9984e03
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2022-05-23 21:34:11 +0530

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -25,3 +25,45 @@ void display_board(){
     cout<<"\t\t\t\t  "<<Board[2][0]<<"  |  "<<Board[2][1]<<"  |  "<<Board[2][2]<<" \n";
     cout<<"\t\t\t\t     |     |     \n";
 }
+void player_turn(){
+    if(turn == 'X'){
+        cout<<"\ntPlayer - 1 [X] turn : ";
+    }
+    else if(turn == 'O'){
+        cout<<"\ntPlayer - 2 [O] turn : ";
+    }
+
+    cin>> choice;
+
+
+
+    switch(choice){
+        case 1: row=0; column=0; break;
+        case 2: row=0; column=1; break;
+        case 3: row=0; column=2; break;
+        case 4: row=1; column=0; break;
+        case 5: row=1; column=1; break;
+        case 6: row=1; column=2; break;
+        case 7: row=2; column=0; break;
+        case 8: row=2; column=1; break;
+        case 9: row=2; column=2; break;
+        default:
+            cout<<"Invalid Move";
+    }
+
+    if(turn == 'X' && Board[row][column] != 'X' && Board[row][column] != 'O'){
+
+        Board[row][column] = 'X';
+        turn = 'O';
+    }else if(turn == 'O' && Board[row][column] != 'X' && Board[row][column] != 'O'){
+
+        Board[row][column] = 'O';
+        turn = 'X';
+    }else {
+
+        cout<<"Box already filled!\n Please choose another!!\n\n";
+        player_turn();
+    }
+
+    display_board();
+}
